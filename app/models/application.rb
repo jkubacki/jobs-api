66a22126840a43ae5b0f1fcdd48ae1262c9cd777
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-11 18:31:36 +0200

    Add associations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,6 @@
 class Application < ApplicationRecord
   belongs_to :job
+  has_many :replies, dependent: :destroy
 
   validates :preference, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
 end
