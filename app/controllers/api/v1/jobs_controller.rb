78f55aa22a31411f80cd6a30929636c3b22516b4
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-12 01:02:29 +0200

    Create job endpoint

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -4,4 +4,36 @@ class Api::V1::JobsController < Api::V1::BaseController
 
     render json: JobBlueprint.render(jobs), status: :ok
   end
+
+  def create
+    job = Job.new(job_params)
+
+    if job.save
+      render json: JobBlueprint.render(job), status: :created
+    else
+      render json: { errors: job.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def job_params # rubocop:disable Metrics/MethodLength
+    params.require(:job).permit(
+      :company,
+      :url,
+      :title,
+      :description,
+      :product,
+      :based_in,
+      :timezones,
+      :stack,
+      :compensation,
+      :pto,
+      :remote,
+      :glassdoor_url,
+      :glassdoor_rating,
+      :notes,
+      :preference
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :jobs, only: [:index]
+      resources :jobs, only: [:index, :create]
     end
   end
 end
