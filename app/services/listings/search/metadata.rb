06731da95becf43dc8aae056c85b44c79b0ae638
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-16 10:43:54 +0200

    Fix to metadata to not be higher than total count

--- app/services/listings/search/metadata.rb
+++ app/services/listings/search/metadata.rb
@@ -6,7 +6,7 @@ module Listings
           total: search.total_count,
           page: search.current_page,
           from: search.offset_value,
-          to: search.offset_value + search.limit_value
+          to: [search.offset_value + search.limit_value, search.total_count].min
         }
       end
     end
