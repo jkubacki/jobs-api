6ae49542b1491ef94604696960ad67312fadecd5
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-16 11:44:14 +0200

    Update listing endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :listings, only: [:index, :create, :destroy]
+      resources :listings, only: [:index, :create, :destroy, :update]
     end
   end
 end
