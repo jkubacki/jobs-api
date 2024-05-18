daa8a5aed00c3c90190b00be4aa04508eec3cd0f
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-18 23:27:53 +0200

    Order listings from the newest by applied_at and created_at

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,5 +5,7 @@ class Application < ApplicationRecord
   has_many :tasks, through: :replies
   has_many :offers, through: :replies
 
+  default_scope { order(applied_at: :desc, created_at: :desc) }
+
   validates :preference, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
 end
