fb74fb0b53a02d5ab9ee147008f289c2197854af
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-21 22:51:56 +0200

    Create elasticsearch.rb

---
+++ config/initializers/elasticsearch.rb
@@ -0,0 +1,14 @@
+if ENV['ELASTICSEARCH_USER'].present?
+  if defined?(Searchkick)
+    Searchkick.client_options = {
+      transport_options: {
+        ssl: {
+          verify: false
+        }
+      }
+    }
+  end
+  ENV['ELASTICSEARCH_URL'] = "https://#{ENV['ELASTICSEARCH_USER'].strip}:#{ENV['ELASTICSEARCH_PASSWORD'].strip}@#{ENV['ELASTICSEARCH_HOST'].strip}:#{ENV['ELASTICSEARCH_PORT'].strip}"
+else
+  ENV['ELASTICSEARCH_URL'] = "#{ENV['ELASTICSEARCH_HOST'] || 'localhost'}:9200}"
+end
