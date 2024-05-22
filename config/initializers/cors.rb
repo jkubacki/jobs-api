5df5479a15f9fe7d2bed371d76d1074563a2c51e
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-22 18:32:37 +0200

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3001", 'jobs.jakubkubacki.com'
+    origins "localhost:3001", 'jobs.jakubkubacki.com', 'demo.jakubkubacki.com'
 
     resource "*",
       headers: :any,
