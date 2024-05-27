39cda2020fd18a22d93d43e91da7e265fd2ea77f
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-27 13:01:10 +0200

    Handle 404 in all controllers

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::BaseController < ApplicationController
 
     render json: { message: "Plase, don't use obscenity." }, status: :unprocessable_entity
   end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    render json: { message: "Not found" }, status: :not_found
+  end
 end
