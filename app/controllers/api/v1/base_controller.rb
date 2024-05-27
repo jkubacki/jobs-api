a83a1446a1de81e5eb90cb6bf189e3879c5b8005
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-27 13:32:09 +0200

    Pass hash to ParamsIncludeObscenity

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BaseController < ApplicationController
   def filter_obscenity
-    return unless ParamsIncludeObscenity.call(params:)
+    return unless ParamsIncludeObscenity.call(params: params.permit!.to_h)
 
     render json: { message: "Plase, don't use obscenity." }, status: :unprocessable_entity
   end
--- app/services/params_include_obscenity.rb
+++ app/services/params_include_obscenity.rb
@@ -1,6 +1,6 @@
 class ParamsIncludeObscenity < ApplicationService
   def call(params:)
-    params.permit!.to_h.any? do |_key, value|
+    params.any? do |_key, value|
       Obscenity.profane?(value.to_s)
     end
   end
