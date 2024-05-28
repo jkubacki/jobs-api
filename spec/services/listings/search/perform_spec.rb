dd94cd2e011b3bab9b967f770d99af44b6b1c06d
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-28 17:33:16 +0200

    Update Listings::Search::Perform tests to include rejected param

--- spec/services/listings/search/perform_spec.rb
+++ spec/services/listings/search/perform_spec.rb
@@ -1,24 +1,44 @@
 require "rails_helper"
 # rubocop:disable RSpec/MultipleMemoizedHelpers
 RSpec.describe Listings::Search::Perform do
-  subject { described_class.call(query:, page:, remote:, per_page:) }
+  subject { described_class.call(query:, page:, remote:, per_page:, rejected:) }
 
   let(:query) { "" }
   let(:page) { "" }
   let(:remote) { "" }
-  let(:per_page) { 3 }
+  let(:per_page) { 10 }
+  let(:rejected) { nil }
 
   let!(:onsite_ruby_title_listing) { create(:listing, remote: false, title: "Ruby on Rails Developer") }
-  let!(:remote_listing) { create(:listing, remote: true) }
-  let!(:onsite_ruby_stack_listing) { create(:listing, remote: false, stack: "Ruby, Typescript") }
+  let!(:rejected_onsite_ruby_title_listing) do
+    create(:listing, :rejected, remote: false, title: "Rejected Ruby on Rails Developer")
+  end
 
-  before { Listing.reindex }
+  let!(:remote_listing) { create(:listing, remote: true) }
+  let!(:rejected_remote_listing) { create(:listing, :rejected, remote: true) }
+
+  let!(:onsite_ruby_stack_listing) { create(:listing, remote: false, stack: "Ruby, Typescript") }
+  let!(:rejected_onsite_ruby_stack_listing) do
+    create(:listing, :rejected, remote: false, stack: "Ruby, Typescript")
+  end
+
+  before do
+    Listing.reindex
+  end
 
   context "when all parameters are empty" do
     let(:per_page) { 2 }
 
-    it "returns first page of the newest listings" do
+    it "returns first page of not rejected, newest listings" do
       expect(subject.value!.results).to eq [onsite_ruby_stack_listing, remote_listing]
+    end
+
+    context "with rejected param" do
+      let(:rejected) { "true" }
+
+      it "returns first page of all newest listings" do
+        expect(subject.value!.results).to eq [rejected_onsite_ruby_stack_listing, onsite_ruby_stack_listing]
+      end
     end
   end
 
@@ -26,32 +46,74 @@ RSpec.describe Listings::Search::Perform do
     let(:page) { 2 }
     let(:per_page) { 2 }
 
-    it "returns the second page of newest listings" do
+    it "returns the second page of not rejected, newest listings" do
       expect(subject.value!.results).to eq [onsite_ruby_title_listing]
+    end
+
+    context "with rejected param" do
+      let(:rejected) { "true" }
+
+      it "returns the second page of all newest listings" do
+        expect(subject.value!.results).to eq [rejected_remote_listing, remote_listing]
+      end
     end
   end
 
   context 'when remote is set to "true"' do
     let(:remote) { "true" }
 
-    it "returns only remote listings" do
+    it "returns not rejected, remote listings" do
       expect(subject.value!.results).to eq [remote_listing]
+    end
+
+    context "with rejected param" do
+      let(:rejected) { "true" }
+
+      it "returns all remote listings" do
+        expect(subject.value!.results).to eq [rejected_remote_listing, remote_listing]
+      end
     end
   end
 
   context 'when remote is set to "false"' do
     let(:remote) { "false" }
 
-    it "returns only on site listings" do
+    it "returns not rejected, on site listings" do
       expect(subject.value!.results).to eq [onsite_ruby_stack_listing, onsite_ruby_title_listing]
+    end
+
+    context "with rejected param" do
+      let(:rejected) { "true" }
+
+      it "returns on site listings" do # rubocop:disable RSpec/ExampleLength
+        expect(subject.value!.results).to eq(
+          [
+            rejected_onsite_ruby_stack_listing, onsite_ruby_stack_listing,
+            rejected_onsite_ruby_title_listing, onsite_ruby_title_listing
+          ]
+        )
+      end
     end
   end
 
   context "when query is set" do
     let(:query) { "ruby" }
 
-    it "returns only listings that match the query" do
+    it "returns not rejected listings that match the query" do
       expect(subject.value!.results).to eq [onsite_ruby_stack_listing, onsite_ruby_title_listing]
+    end
+
+    context "with rejected param" do
+      let(:rejected) { "true" }
+
+      it "returns listings that match the query" do # rubocop:disable RSpec/ExampleLength
+        expect(subject.value!.results).to eq(
+          [
+            rejected_onsite_ruby_stack_listing, onsite_ruby_stack_listing,
+            rejected_onsite_ruby_title_listing, onsite_ruby_title_listing
+          ]
+        )
+      end
     end
   end
 
