aa90c60a62c61454c30d2a3126e4eefbc374fddd
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-28 19:20:28 +0200

    Consider modified replies as changes

--- app/services/seeds/any_changes.rb
+++ app/services/seeds/any_changes.rb
@@ -1,10 +1,12 @@
 module Seeds
   class AnyChanges < ApplicationService
-    def call(expected_listings_count: 13, expected_applications_count: 4)
+    def call(expected_listings_count: 13, expected_applications_count: 5, expected_replies_count: 13)
       Listing.count != expected_listings_count ||
         Application.count != expected_applications_count ||
+        Reply.count != expected_replies_count ||
         Listing.where("created_at != updated_at").any? ||
-        Application.where("created_at != updated_at").any?
+        Application.where("created_at != updated_at").any? ||
+        Reply.where("created_at != updated_at").any?
     end
   end
 end
--- spec/services/seeds/any_changes_spec.rb
+++ spec/services/seeds/any_changes_spec.rb
@@ -1,11 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Seeds::AnyChanges do
-  subject { described_class.call(expected_listings_count: 1, expected_applications_count: 1) }
+  subject do
+    described_class.call(expected_listings_count: 1, expected_applications_count: 1, expected_replies_count: 1)
+  end
 
   before do
     listing = create(:listing)
-    create(:application, listing:)
+    application = create(:application, listing:)
+    create(:reply, application:)
   end
 
   context "when there are no changes" do
@@ -44,6 +47,24 @@ RSpec.describe Seeds::AnyChanges do
 
   context "when application was updated" do
     before { Application.first.update(notes: "New notes") }
+
+    it { is_expected.to eq true }
+  end
+
+  context "when there is a new reply" do
+    before { create(:reply, application: Application.first) }
+
+    it { is_expected.to eq true }
+  end
+
+  context "when reply was deleted" do
+    before { Reply.first.destroy }
+
+    it { is_expected.to eq true }
+  end
+
+  context "when reply was updated" do
+    before { Reply.first.update(notes: "New notes") }
 
     it { is_expected.to eq true }
   end
