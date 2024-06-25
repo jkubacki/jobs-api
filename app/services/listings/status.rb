f702e36e475c78f130bfea4c89798c86f49c6e1d
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-25 11:23:51 +0200

    Listing::Status service

---
+++ app/services/listings/status.rb
@@ -0,0 +1,18 @@
+module Listings
+  class Status < ApplicationService
+    PENDING = "pending".freeze
+    APPLIED = "applied".freeze
+    REJECTED = "rejected".freeze
+    IN_PROGRESS = "in_progress".freeze
+
+    def call(listing:)
+      applications = listing.applications.includes(:replies)
+
+      return PENDING if applications.empty?
+      return APPLIED if applications.all? { |application| application.replies.empty? }
+      return REJECTED if applications.any? { |application| application.replies.any?(&:rejected?) }
+
+      IN_PROGRESS
+    end
+  end
+end
---
+++ spec/services/listings/status_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+describe Listings::Status do
+  subject { described_class.call(listing:) }
+
+  let(:listing) { create(:listing) }
+
+  context "when listing has no applications" do
+    it "returns pending" do
+      expect(subject).to eq(described_class::PENDING)
+    end
+  end
+
+  context "when listing has applications" do
+    context "with no replies" do
+      before do
+        create(:application, listing:)
+      end
+
+      it "returns applied" do
+        expect(subject).to eq(described_class::APPLIED)
+      end
+    end
+
+    context "with some rejected replies and some not rejected" do
+      before do
+        application = create(:application, listing:)
+        create(:reply, application:, rejected: false)
+        create(:reply, application:, rejected: true)
+      end
+
+      it "returns rejected" do
+        expect(subject).to eq(described_class::REJECTED)
+      end
+    end
+
+    context "without any rejected replies" do
+      before do
+        application = create(:application, listing:)
+        create(:reply, application:, rejected: false)
+      end
+
+      it "returns in progress" do
+        expect(subject).to eq(described_class::IN_PROGRESS)
+      end
+    end
+  end
+end
